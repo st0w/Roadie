aa9140faa299c05ab9395094c56a64115beb3744
st0w <st0w@st0w.com>
2011-02-14 10:04:05 -0500

    Offer both white and blacklisting of extensions to use

--- sync_directory_hierarchy_with_itunes.py
+++ sync_directory_hierarchy_with_itunes.py
@@ -13,40 +13,96 @@ Note that this only looks at the actual filename/path.  It doesn't check
 for duplicate status or anything like that.  Just finds files below a
 given directory and adds them to iTunes.
 
+Certain files are excluded based upon their extension.  Change the
+definition of IGNORE_EXTENSIONS to alter this.
+
 """
 # ---*< Standard imports >*----------------------------------------------------
 import os
+import re
 import sys
 
 # ---*< Third-party imports >*-------------------------------------------------
 from appscript import k, CommandError
+from mactypes import Alias
 
 # ---*< Local imports >*-------------------------------------------------------
-from itunes import ITunesManager
+from itunes import ITunesManager, smart_str
 
 # ---*< Initialization >*------------------------------------------------------
 DEFAULT_DIR = '/srv/multimedia/Music'
 EXCLUDE_DIRS = ['/srv/multimedia/Music/Production',
+                '/srv/multimedia/Music/iTunes/Previous iTunes Libraries',
                 ]
+IGNORE_EXTENSIONS = re.compile('^.+\.('
+                               '(accurip)'
+                               '|(asd)'
+                               '|(avi)'
+                               '|(cue)'
+                               '|(dctmp)'
+                               '|(dmg)'
+                               '|(dylib)'
+                               '|(epub)'
+                               '|(exe)'
+                               '|(flac)'
+                               '|(gif)'
+                               '|(gz)'
+                               '|(htm)'
+                               '|(html)'
+                               '|(ipa)'
+                               '|(iso)'
+                               '|(itc)'
+                               '|(itdb)'
+                               '|(jpeg)'
+                               '|(jpg)'
+                               '|(log)'
+                               '|(m3u)'
+                               '|(m4v)'
+                               '|(md5)'
+                               '|(mov)'
+                               '|(nfo)'
+                               '|(pls)'
+                               '|(rar)'
+                               '|(r\d\d)'
+                               '|(rtf)'
+                               '|(sfv)'
+                               '|(pdf)'
+                               '|(plist)'
+                               '|(png)'
+                               '|(rss)'
+                               '|(so)'
+                               '|(tgz)'
+                               '|(tif)'
+                               '|(tiff)'
+                               '|(tmp)'
+                               '|(torrent)'
+                               '|(txt)'
+                               '|(url)'
+                               '|(xml)'
+                               '|(zip)'
+                               ')$', re.IGNORECASE)
+INCLUDE_EXTENSIONS = re.compile('^.+\.('
+                               '(m4a)|(m4p)|(mp2)|(mp3)|(mp4)|(wav)'
+                               ')$', re.IGNORECASE)
 
 # ---*< Code >*----------------------------------------------------------------
 
-def sync_dir(dirname, recursive=True):
-    """Synchronizes a directory hierarchy with iTunes
+def sync_dir(dirname):
+    """Recursively synchronizes a directory hierarchy with iTunes
     
     :param dirname: `string` of the root directory
-    :param recursive: `bool` whether or not to recursively sync
     """
     itunes = ITunesManager()#IGNORE:C0103
     tracks = itunes.get_all_tracks()
 
     itunes_files = []
     new_files = []
+#    unused_new_files = []
     i = 0
     count = len(tracks)
 
     """Now traverse the desired file hierarchy and add files"""
-    print "Walking dir tree..."
+    sys.stdout.write("Walking dir tree(s)...")
     for root, dirs, files in os.walk(dirname):
         skipping = False
         for skipdir in EXCLUDE_DIRS:
@@ -56,19 +112,32 @@ def sync_dir(dirname, recursive=True):
         if skipping:
             continue
 
-#        sys.stderr.write('\r%s        ' % root)
         if '.DS_Store' in files:
             files.remove('.DS_Store')
 
-        new_files += ['%s/%s' % (root, f) for f in files]
+#        """This will add files using a blacklist - every extension not
+#        in the IGNORE_EXTENSIONS regex
+#        """ 
+#        unused_new_files += ['%s/%s' % (root, f) for f in files if not
+#                      re.match(IGNORE_EXTENSIONS, f)]
 
-    print "done"
-    print len(new_files)
-    print new_files
-    sys.exit(0)
+        """This adds files using a whitelist - only known extensions set
+        in the INCLUDE_EXTENSIONS regex
+        """
+        new_files += ['%s/%s' % (root, f) for f in files if
+                      re.match(INCLUDE_EXTENSIONS, f)]
+
+    sys.stdout.write("done.  Identified %d files.\n" % len(new_files))
+#    print len(new_files)
+#    print new_files
+#    for f in new_files:
+##        if not re.match(INCLUDE_EXTENSIONS, f):
+#        print f
+#    sys.exit(0)
 
 
     """Build a list containing full paths to all files in iTunes library"""
+    sys.stdout.write("Examining existing iTunes Library...\n")
     for t in tracks:
         i += 1
         sys.stderr.write('\rProcessing %s/%s' % (i, count))
@@ -80,9 +149,56 @@ def sync_dir(dirname, recursive=True):
             sys.stderr.write('\nOops... Error getting location for %s: %s\n' %
                              (t.name(), str(e)))
 
+    sys.stdout.write("\nDone.  Now removing existing entries from list of "
+                     "files to add...\n")
 
-    print "\n%s" % dirname
-    print len(itunes_files)
+    """I thought about the best way to preen the results, analyzing the
+    two lists and such.  Then I decided that more often than not, the
+    hierarchy would contain more files than the iTunes library.  The
+    assumption being, the script is being run to sync the new files in
+    the hierarchy and thus includes all of the iTunes library plus more.
+    
+    Note that I just try to remove it and then catch any exceptions...
+    rather than doing a lookup and only removing existing elements.  I
+    don't know for sure if this is faster, but, it seems like it would be.  
+    """
+    for f in itunes_files:
+        try:
+            new_files.remove(f)
+        except ValueError:
+            pass
+
+    sys.stdout.write('Post processing, preparing to add %d new files to '
+                     'existing library of %d files...\n'
+                     % (len(new_files), len(itunes_files)))
+
+    """This is where the files are actually added to iTunes
+    
+    With a large import... this is SSLLLLLOOOOOOWWWWWWW... First tried
+    to add one at a time, but that came to a grinding halt around 100.
+    Then tried all at once, and that resulted in a timeout.
+    """
+    aliases = []
+    for f in new_files:
+#        itunes.itunes.add(Alias(f))
+        try:
+            aliases.append(Alias(smart_str(f)))
+        except UnicodeDecodeError:
+            sys.stderr.write('Error trying to Alias/append %s\n' % smart_str(f))
+
+    sys.stdout.write('Now adding %d files to iTunes library...' % len(aliases))
+    try:
+        itunes.itunes.add(aliases)
+    except CommandError as e:
+        sys.stderr.write('Warning: An error occurred trying to add the files.'
+                         '\nPlease note that if the error is "Apple event '
+                         'timed out" that iTunes may very well still be '
+                         '\nprocessing.  Continue to check iTunes and see if '
+                         'this is the case.  If all your files are added, the '
+                         '\nerror can be safely ignored.\n%s' % str(e))
+
+    sys.stdout.write('Done.\n')
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
